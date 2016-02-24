2018029c0a5c824f8c58a43758d29e2658f1cf05
arowla <arowland@doc.gov>
2016-02-24 14:24:21 -0500

    Added /api/trips endpoint with Restless
    - TODO: Need to serialize related models

--- principal_travel/urls.py
+++ principal_travel/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from travel.views import TripList
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/trips', TripList.as_view(), name='trip_list'),
 ]
--- travel/views.py
+++ travel/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import EventLocationPrincipalTravel
+from restless.views import Endpoint
+from restless.models import serialize
 
 # Create your views here.
+class TripList(Endpoint):
+    def get(self, request):
+        all = EventLocationPrincipalTravel.objects.all()
+        return serialize(all)
+
+
