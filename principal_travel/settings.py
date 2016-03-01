96390e00aa1fbe75e0fb7672c2081c1c36d0c872
noonkay <negar.kalbasi@gmail.com>
2016-03-01 13:57:06 -0500

    added django-registration-redux

--- principal_travel/settings.py
+++ principal_travel/settings.py
@@ -33,12 +33,14 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
     'travel',
+    'registration',
 ]
 
 REST_FRAMEWORK = {
