64c8ecc336dc95b7cd8638f5d190ecef46b490e2
arowla <arowland@doc.gov>
2016-03-03 16:05:14 -0500

    Added the U.S. Federal Web Design Standards
    - using the django-designstandards app

--- travel/views.py
+++ travel/views.py
@@ -1,8 +1,17 @@
 from .models import EventLocationPrincipalTravel
 from .serializers import ELPTSerializer
 from rest_framework import viewsets
+from django.views.generic import TemplateView
+from django.template.loader import get_template
 
 # Create your views here.
 class TripViewSet(viewsets.ModelViewSet):
     queryset = EventLocationPrincipalTravel.objects.all()
     serializer_class = ELPTSerializer
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        return context
