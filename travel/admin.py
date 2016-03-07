78fb1915ba1fd41ac1250421657e663c2892a71f
Sasan Bahadaran <sbahadaran@doc.gov>
2016-03-07 16:50:30 -0500

    added older migration files and changes to admin.py

--- travel/admin.py
+++ travel/admin.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Event, Location, Principal, Travel, EventLocationPrincipalTravel
+from .models import Event, Location, Principal, Travel, EventLocationPrincipalTravel, Trip, TripEvent, Region, EventType, Agency, Office, CountryRegion
 
 admin.site.register(Event)
 admin.site.register(Location)
 admin.site.register(Principal)
 admin.site.register(Travel)
 admin.site.register(EventLocationPrincipalTravel)
+admin.site.register(Trip)
+admin.site.register(TripEvent)
+admin.site.register(Region)
+admin.site.register(EventType)
+admin.site.register(Agency)
+admin.site.register(Office)
+admin.site.register(CountryRegion)
