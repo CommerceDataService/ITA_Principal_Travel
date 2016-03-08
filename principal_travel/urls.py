4af6f28704def7b2f701f938e78fc807f539f478
arowla <arowland@doc.gov>
2016-03-08 12:20:24 -0500

    Added the Django debug toolbar
    - and made it always-on for the dev environment

--- principal_travel/urls.py
+++ principal_travel/urls.py
@@ -13,7 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls import url, include, patterns
 from django.contrib import admin
 from travel.views import TripViewSet
 from rest_framework import routers
@@ -28,3 +29,8 @@ urlpatterns = [
     # url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^$', 'travel.views.home', name='home')
 ]
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
