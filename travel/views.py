fa1ffed2060ae25b47f87ce3154eb491778fb7f8
Sasan Bahadaran <sbahadaran@doc.gov>
2016-03-09 14:46:09 -0500

    removed references to outdated models

--- travel/views.py
+++ travel/views.py
@@ -1,8 +1,3 @@
-from .models import EventLocationPrincipalTravel
-from .serializers import ELPTSerializer
 from rest_framework import viewsets
 
 # Create your views here.
-class TripViewSet(viewsets.ModelViewSet):
-    queryset = EventLocationPrincipalTravel.objects.all()
-    serializer_class = ELPTSerializer
