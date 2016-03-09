52c58afa0a7f6100d338296c87d47e4e3e20b6c3
Sasan Bahadaran <sbahadaran@doc.gov>
2016-03-09 15:41:14 -0500

    added missing home view

--- travel/views.py
+++ travel/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    print(request.user)
+    return render(request, 'travel/home.html', {'current_user': request.user})
