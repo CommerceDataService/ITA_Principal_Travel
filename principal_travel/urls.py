2500fa4628fa0c181ba86ace290e2014703534fe
Sasan Bahadaran <sbahadaran@doc.gov>
2016-03-09 14:41:48 -0500

    removed references to outdated models

--- principal_travel/urls.py
+++ principal_travel/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from travel.views import TripViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r'trips', TripViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
