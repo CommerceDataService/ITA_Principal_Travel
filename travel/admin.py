8a45f568734c29505c3db52f868271b36e7b01f2
Sasan Bahadaran <sbahadaran@doc.gov>
2016-03-11 13:13:56 -0500

    changed registration module to allow for manual activation

--- travel/admin.py
+++ travel/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
+#from registration.models import RegistrationManager
 
 # Register your models here.
 
 from .models import Event, Principal, Trip, Region, EventType, Agency, Office
+
+#def make_active(modeladmin, request, queryset):
+#    queryset.update(activated= true)
+#make_published.short_description = "Activate selected users"
+
+#class RegistrationActivation(admin.ModelAdmin):
+#    list_display = ['username', 'status']
+#    ordering = ['username']
+#    actions = [make_active]
 
 admin.site.register(Event)
 admin.site.register(Principal)
@@ -11,3 +21,7 @@ admin.site.register(Region)
 admin.site.register(EventType)
 admin.site.register(Agency)
 admin.site.register(Office)
+#admin.site.register(RegistrationManager)
+#admin.site.register(RegistrationProfile,RegistrationActivation)
+
+
