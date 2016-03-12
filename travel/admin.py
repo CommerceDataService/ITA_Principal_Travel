8fc6cd106c2d84df96a8303db48f5909b87f8006
Sasan Bahadaran <sbahadaran@doc.gov>
2016-03-12 14:48:59 -0500

    changed behavior of admin for registration and linked admin panel menu button to admin dashboard

--- travel/admin.py
+++ travel/admin.py
@@ -1,18 +1,31 @@
 from django.contrib import admin
-#from registration.models import RegistrationManager
-
-# Register your models here.
-
+from registration.models import RegistrationProfile
+from registration.users import UsernameField
+from django.utils.translation import ugettext_lazy as _
 from .models import Event, Principal, Trip, Region, EventType, Agency, Office
 
-#def make_active(modeladmin, request, queryset):
-#    queryset.update(activated= true)
-#make_published.short_description = "Activate selected users"
+class RegistrationReduxAdmin(admin.ModelAdmin):
+    actions = ['activate_users']
+    list_display = ('user','activated')
+    raw_id_fields = ['user']
+    search_fields = ('user__{0}'.format(UsernameField()),
+                     'user__first_name', 'user__last_name')
+    
+    def activate_users(self, request, queryset):
+        """
+        Activates the selected users, if they are not already
+        activated.
+        """
+        for profile in queryset:
+            RegistrationProfile.objects.activate_user(profile.activation_key)
 
-#class RegistrationActivation(admin.ModelAdmin):
-#    list_display = ['username', 'status']
-#    ordering = ['username']
-#    actions = [make_active]
+            profile.delete()
+
+    activate_users.short_description = _("Activate users")
+
+#unregister admin model and register it again to use new admin model
+admin.site.unregister(RegistrationProfile)
+admin.site.register(RegistrationProfile,RegistrationReduxAdmin)
 
 admin.site.register(Event)
 admin.site.register(Principal)
@@ -21,7 +34,5 @@ admin.site.register(Region)
 admin.site.register(EventType)
 admin.site.register(Agency)
 admin.site.register(Office)
-#admin.site.register(RegistrationManager)
-#admin.site.register(RegistrationProfile,RegistrationActivation)
 
 
