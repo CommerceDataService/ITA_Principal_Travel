4b07acf10109dbe1a7827184ff6af7fe63338d30
noonkay <negar.kalbasi@gmail.com>
2016-03-25 16:54:23 -0400

    Refactored script.js and added dropdown for toggling bar charts

--- principal_travel/static/js/script.js
+++ principal_travel/static/js/script.js
@@ -1,4 +1,4 @@
-function compare(a, b) {
+var compare = function(a, b) {
     if (a[1] === b[1]) {
         return 0;
     }
@@ -64,39 +64,22 @@ var pairSortSlice = function(object){
   return array.slice(0,10)
 }
 
-$(document).ready(function(){
+var renderTable = function(tripData){
+  var sortedTrips = _(tripData).chain().sortBy(function(trip){
+    return start = new Date(trip.start_date)
+  }).value()
 
-  $.getJSON('/api/events')
-  .done(function(data){
-
-    var countryCount = _.countBy(data, function(event){
-      return event.cities_light_country.name;
-    })
-    drawBarGraph(pairSortSlice(countryCount));
-
+  _.each(sortedTrips, function(trip){
+    var now = new Date();
+    var start = new Date(trip.start_date)
+    if(start>now){
+      var template = Handlebars.compile($('#table').html())
+      $('#time-table').append(template(trip))
+    }
   })
+}
 
-  $.getJSON('/api/trips')
-  .done(function(data){
-    var principalCount = _.countBy(data, function(trip){
-      return trip.principal.title;
-    })
-    drawBarGraph(pairSortSlice(principalCount));
-
-    var sortedTrips = _(data).chain().sortBy(function(trip){
-      return start = new Date(trip.start_date)
-    }).value()
-
-    _.each(sortedTrips, function(trip){
-      var now = new Date();
-      var start = new Date(trip.start_date)
-      if(start>now){
-        var template = Handlebars.compile($('#table').html())
-        $('#time-table').append(template(trip))
-      }
-    })
-  })
-
+var renderMap = function(tripData){
   var map = L.map('map')
   .setView([33, -8], 2);
 
@@ -114,39 +97,68 @@ $(document).ready(function(){
   yearMarkers = L.layerGroup([]).addTo(map);
   allPastMarkers = L.layerGroup([]).addTo(map);
 
-  $.getJSON('/api/trips').done(function(data){
-    var today = new Date();
-    var monthAgo = new Date().setDate(today.getDate()-30)
-    var yearAgo = new Date().setDate(today.getDate()-365)
-    var template = Handlebars.compile($('#popup').html());
-    _.each(data, function(trip){
-      var start = new Date(trip.start_date)
-      _.each(trip.events, function(event){
-        var marker = L.marker([event.cities_light_city.latitude, event.cities_light_city.longitude]).bindPopup(template(event));
-        if(start >= today){
-          futureMarkers.addLayer(marker);
-        }else {
-          if (start<today && start>= monthAgo) {
-            monthMarkers.addLayer(marker);
-          }
-          if (start<today && start >= yearAgo) {
-            yearMarkers.addLayer(marker);
-          }
-          allPastMarkers.addLayer(marker);
+  var today = new Date();
+  var monthAgo = new Date().setDate(today.getDate()-30)
+  var yearAgo = new Date().setDate(today.getDate()-365)
+  var template = Handlebars.compile($('#popup').html());
+  _.each(tripData, function(trip){
+    var start = new Date(trip.start_date)
+    _.each(trip.events, function(event){
+      var marker = L.marker([event.cities_light_city.latitude, event.cities_light_city.longitude]).bindPopup(template(event));
+      if(start >= today){
+        futureMarkers.addLayer(marker);
+      }else {
+        if (start<today && start>= monthAgo) {
+          monthMarkers.addLayer(marker);
         }
-      })
+        if (start<today && start >= yearAgo) {
+          yearMarkers.addLayer(marker);
+        }
+        allPastMarkers.addLayer(marker);
+      }
     })
+  })
 
-    $( "input" ).click(function( event ) {
-        var layerClicked = window[event.target.value];
-            console.log(window)
-            console.log(layerClicked)
-            if (map.hasLayer(layerClicked)) {
-                map.removeLayer(layerClicked);
-            }
-            else{
-                map.addLayer(layerClicked);
-            } ;
-    });
+  $( "input" ).on('click', function( event ) {
+    var layerClicked = window[event.target.value];
+    if (map.hasLayer(layerClicked)) {
+      map.removeLayer(layerClicked);
+    } else {
+      map.addLayer(layerClicked);
+    }
+  })
+}
+
+var renderChart = function(tripData, eventData){
+  var principalCount = _.countBy(tripData, function(trip){
+    return trip.principal.title;
+  })
+  var countryCount = _.countBy(eventData, function(event){
+    return event.cities_light_country.name;
+  })
+  drawBarGraph(pairSortSlice(countryCount));
+  $("#options").on('change', function(event){
+    if (this.value == "country"){
+      $('#chart').empty().append('<h3>Top Destinations</h3>')
+      drawBarGraph(pairSortSlice(countryCount));
+    } else if (this.value == "principal"){
+      $('#chart').empty().append('<h3>Top Travelers</h3>')
+      drawBarGraph(pairSortSlice(principalCount));
+    }
+  })
+}
+
+$(document).ready(function(){
+  var tripData, eventData;
+  $.getJSON('/api/events')
+  .done(function(data){
+    eventData = data;
+    $.getJSON('/api/trips')
+    .done(function(data){
+      tripData = data;
+      renderTable(tripData);
+      renderChart(tripData, eventData);
+      renderMap(tripData);
+    })
   })
 })
