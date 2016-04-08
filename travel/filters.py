1c123c3c511f1c5645b819d2e195f6fc656b61eb
noonkay <negar.kalbasi@gmail.com>
2016-04-08 15:56:11 -0400

    Added filter template

--- travel/filters.py
+++ travel/filters.py
@@ -1,16 +1,14 @@
 import django_filters
+from .models import Trip, Event
 
 class TripFilter(django_filters.FilterSet):
     start_date = django_filters.DateFromToRangeFilter()
     events = django_filters.ModelMultipleChoiceFilter(name ='events__name', to_field_name='name', lookup_type='in', queryset = Event.objects.all())
 
-    event_type = django_filters.ModelMultipleChoiceFilter(name ='events__event_type__name', to_field_name='event_type__name', lookup_type='in', queryset = Event.objects.all())
-
-    start_date__month = django_filters.DateFilter(lookup_type='month')
-    start_date__year= django_filters.DateFilter(lookup_expr='year')
+    # event_type = django_filters.ModelMultipleChoiceFilter(name ='events__event_type__name', to_field_name='event_type__name', lookup_type='in', queryset = Event.objects.all())
 
     class Meta:
         model = Trip
         fields = {
-        'principal__title': ['exact', 'icontains'],
+        'principal__title': ['icontains'],
         }
