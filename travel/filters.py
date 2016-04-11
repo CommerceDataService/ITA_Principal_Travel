e0dacdd94fa3c715eea0bf506cab124bf5b5dc46
noonkay <negar.kalbasi@gmail.com>
2016-04-11 16:37:36 -0400

    Corrected casing on filter attributes in filters.py

--- travel/filters.py
+++ travel/filters.py
@@ -1,16 +1,17 @@
 import django_filters
 from .models import Trip, Event, Region, EventType
 from cities_light.models import Country
+from datetimewidget.widgets import DateWidget
 
 class TripFilter(django_filters.FilterSet):
-    Principal_title = django_filters.CharFilter(lookup_expr='icontains', name='principal__title')
-    Quick_Dates = django_filters.DateRangeFilter()
-    Custom_Date_Range = django_filters.DateFromToRangeFilter()
-    Month = django_filters.NumberFilter(name='start_date__month')
-    Year = django_filters.NumberFilter(name='start_date__year')
-    Region = django_filters.ModelChoiceFilter(queryset = Region.objects.all(), name='events__cities_light_country__custom_region')
-    Country = django_filters.ModelChoiceFilter(queryset = Country.objects.all(), name='events__cities_light_country__name')
-    Event_Type = django_filters.ModelChoiceFilter(queryset = EventType.objects.all(), name='events__event_type')
+    principal_title = django_filters.CharFilter(lookup_expr='icontains', name='principal__title')
+    quick_dates = django_filters.DateRangeFilter()
+    custom_date_range = django_filters.DateFromToRangeFilter(widget=DateWidget(usel10n=True, bootstrap_version=3))
+    month = django_filters.NumberFilter(name='start_date__month')
+    year = django_filters.NumberFilter(name='start_date__year')
+    region = django_filters.ModelChoiceFilter(queryset = Region.objects.all(), name='events__cities_light_country__custom_region')
+    country = django_filters.ModelChoiceFilter(queryset = Country.objects.all(), name='events__cities_light_country__name')
+    event_type = django_filters.ModelChoiceFilter(queryset = EventType.objects.all(), name='events__event_type')
 
     class Meta:
         model = Trip
