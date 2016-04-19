cfa2c2506c9a3763d32effa5dd6dba3f7e0f56c7
arowla <arowland@doc.gov>
2016-04-19 16:33:20 -0400

    Add conf via env var for DB password and secret key

--- principal_travel/settings/prod.py
+++ principal_travel/settings/prod.py
@@ -1,1 +1,25 @@
 from .base import *
+import os
+from django.core.exceptions import ImproperlyConfigured
+
+
+try:
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+except KeyError as e:
+    raise ImproperlyConfigured("You must define DJANGO_SECRET_KEY")
+
+ALLOWED_HOSTS = ['ec2-52-207-245-215.compute-1.amazonaws.com']
+
+try:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'traveltracker',
+            'USER': 'cdsadmin',
+            'HOST': 'localhost',
+            'PORT': 5432,
+            'PASSWORD': os.environ['DJANGO_DB_PASSWORD'],
+        }
+    }
+except KeyError as e:
+    raise ImproperlyConfigured("You must define DJANGO_DB_PASSWORD.")
