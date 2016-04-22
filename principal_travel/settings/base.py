7b9ae4103feec7e8a6d0fce01d6f492030413e6f
arowla <arowland@doc.gov>
2016-04-22 10:42:03 -0400

    Change path style in settings

--- principal_travel/settings/base.py
+++ principal_travel/settings/base.py
@@ -18,7 +18,7 @@ except:
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.join(os.path.abspath(__file__), '..', '..')
 
 
 # Quick-start development settings - unsuitable for production
