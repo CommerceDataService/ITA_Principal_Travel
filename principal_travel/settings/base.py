a510a6f576b1776293811838074f447eb8664b61
arowla <arowland@doc.gov>
2016-04-22 10:41:10 -0400

    Fix static paths

--- principal_travel/settings/base.py
+++ principal_travel/settings/base.py
@@ -159,8 +159,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_ROOT]
 
 # cities light configuration
 CITIES_LIGHT_TRANSLATION_LANGUAGES = ['en']
