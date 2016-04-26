de959cc0594703d460df6969afb8ace9c282f2e5
arowla <arowland@doc.gov>
2016-04-25 22:29:14 -0400

    Add Content-Length to response header
    - Provided by ConditionalGetMiddleware
    - Content-Length is needed by AWS ELB to check instance health

--- principal_travel/settings/base.py
+++ principal_travel/settings/base.py
@@ -76,6 +76,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
