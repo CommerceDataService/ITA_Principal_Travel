237a58549be8643a368a5a30b5f29cfe71b55037
arowla <arowland@doc.gov>
2016-04-25 23:09:15 -0400

    Fix custom middleware bugs

--- principal_travel/middleware.py
+++ principal_travel/middleware.py
@@ -2,5 +2,6 @@
 AWS ELB needs Content-Length set (and non-zero) on responses to consider an EC2 instance healthy
 '''
 class ContentLengthMiddleware():
-    def process_response(request, response):
+    def process_response(self, request, response):
         response['Content-Length'] = len(response.content)
+        return response
