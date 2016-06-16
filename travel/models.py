488c4535fd9c532db46cc743512f76fadac41c62
arowla <arowland@doc.gov>
2016-06-16 18:00:41 -0400

    Changed region from M2M to One2M relationship
    - Also changed field name from custom_region -> agency_region

--- travel/models.py
+++ travel/models.py
@@ -1,36 +1,31 @@
-#This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from __future__ import unicode_literals
 from django.db import models
-from cities_light.models import Country
-from django.contrib.auth.models import User
 from author.decorators import with_author
 
+
 class TimeStampedModel(models.Model):
-    created_on = models.DateTimeField(auto_now_add = True, null=True)
-    edited_on = models.DateTimeField(auto_now = True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    edited_on = models.DateTimeField(auto_now=True, null=True)
+
     class Meta:
         abstract = True
 
-@with_author #adds and maintains 'author' and 'updated_by' fields
+
+@with_author  # adds and maintains 'author' and 'updated_by' fields
 class Event(TimeStampedModel):
     id = models.AutoField(primary_key=True)
     host = models.CharField(max_length=255, blank=True, null=True)
     name = models.CharField(max_length=255, null=True)
     description = models.TextField(blank=True, null=True)
-    event_type = models.ForeignKey('EventType',null=True)
+    event_type = models.ForeignKey('EventType', null=True)
     press = models.NullBooleanField()
     press_note = models.CharField(max_length=255, blank=True, null=True)
-    cities_light_city = models.ForeignKey('cities_light.City',null=True)
-    cities_light_country = models.ForeignKey('cities_light.Country',null=True)
+    cities_light_city = models.ForeignKey('cities_light.City', null=True)
+    cities_light_country = models.ForeignKey('cities_light.Country', null=True)
 
     def __str__(self):
         return "{}".format(self.name)
+
 
 @with_author
 class Principal(TimeStampedModel):
@@ -44,6 +39,7 @@ class Principal(TimeStampedModel):
 
     def __str__(self):
         return "{}, {}, {}".format(self.first_name, self.last_name, self.title)
+
 
 @with_author
 class Trip(TimeStampedModel):
@@ -67,7 +63,6 @@ class Trip(TimeStampedModel):
         update_event_List = [str(name) for name in event_List]
         return ', '.join(update_event_List)
 
-
     def __str__(self):
         return "{} - {}".format(self.start_date, self.end_date)
 
@@ -76,7 +71,7 @@ class Region(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, blank=False, null=False)
     agency = models.ForeignKey('Agency', null=True)
-    countries = models.ManyToManyField(Country, related_name="custom_region")
+    country = models.ForeignKey('cities_light.Country', related_name='agency_region', null=True)
 
     def __str__(self):
         return "{}".format(self.name)
