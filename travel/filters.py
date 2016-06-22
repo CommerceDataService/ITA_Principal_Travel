c16dd75808e38958e6eafd3911e40263ed1b7116
noonkay <negar.kalbasi@gmail.com>
2016-06-22 19:30:33 -0400

    Resolved merge conflict

--- travel/filters.py
+++ travel/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from .models import Trip, Event, Region, EventType
+from .models import Trip, Event, Region, EventType, Principal
 from cities_light.models import Country
 from datetimewidget.widgets import DateWidget
 
@@ -7,6 +7,7 @@ class TripFilter(django_filters.FilterSet):
     principal_title = django_filters.CharFilter(lookup_expr='icontains', name='principal__title', label='Principal Title')
     principal_name = django_filters.CharFilter(lookup_expr='icontains', name='principal__last_name', label='Principal Lastname')
     quick_dates = django_filters.DateRangeFilter(label='Quick Dates', name='start_date')
+    principal =django_filters.ModelChoiceFilter(queryset = Principal.objects.all(), name='principal')
     date_range_start = django_filters.DateFilter(lookup_expr='gte', name='start_date', widget=DateWidget(usel10n=True, bootstrap_version=3), label='Custom Date Range - From')
     date_range_end = django_filters.DateFilter(lookup_expr='lte', name='start_date', widget=DateWidget(usel10n=True, bootstrap_version=3), label='Custom Date Range - To')
     month = django_filters.NumberFilter(name='start_date__month', help_text='Enter numeric value for month(e.g. 1 for January)')
